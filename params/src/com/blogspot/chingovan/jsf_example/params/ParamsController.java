b6632c0e02f80e49e039119e676f64610e214ea4
ChiNV <chingovan@gmail.com>
2017-11-24 23:19:49 +0700

    Commit

---
+++ params/src/com/blogspot/chingovan/jsf_example/params/ParamsController.java
@@ -0,0 +1,13 @@
+package com.blogspot.chingovan.jsf_example.params;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * Created by ChiNV on 11/24/2017.
+ */
+@ManagedBean(name = "paramsController")
+@RequestScoped
+public class ParamsController {
+
+}
